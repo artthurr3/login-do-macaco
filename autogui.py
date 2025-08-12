9f77a0a19d720b206a57c0e9a01fbcd60e586b93
artthurr3 <gamerxand9@gmail.com>
2025-08-12 11:35:34 -0300

    Add files via upload
    import pyautogui as aut
    import time
    
    import pyautogui as aut
    import time
    
    aut.press('win')
    time.sleep(1)
    aut.write('google chrome')
    aut.press('enter')
    time.sleep(2)
    aut.write('https://www.instagram.com')
    aut.press('enter')
    time.sleep(3)
    aut.click(x=600, y=350)
    time.sleep(2)
    aut.click(x=530, y=400)
    aut.write('macaco_com_aidss')
    time.sleep(3)
    aut.click(x=530, y=450)
    aut.write('11022010___')
    time.sleep(2)
    aut.click(x=600, y=480)

---
+++ autogui.py
@@ -0,0 +1,23 @@
+import pyautogui as aut
+import time
+
+import pyautogui as aut
+import time
+
+aut.press('win')
+time.sleep(1)
+aut.write('google chrome')
+aut.press('enter')
+time.sleep(2)
+aut.write('https://www.instagram.com')
+aut.press('enter')
+time.sleep(3)
+aut.click(x=600, y=350)
+time.sleep(2)
+aut.click(x=530, y=400)
+aut.write('macaco_com_aidss')
+time.sleep(3)
+aut.click(x=530, y=450)
+aut.write('11022010___')
+time.sleep(2)
+aut.click(x=600, y=480)
